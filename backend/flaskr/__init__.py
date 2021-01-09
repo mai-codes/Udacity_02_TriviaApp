8d3f855cecc26587a8a43a1216c35feb92633ecf
mai-codes <maissounksara@gmail.com>
2021-01-09 15:14:04 -0500

    complete POST quiz method

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -207,6 +207,33 @@ def create_app(test_config=None):
   one question at a time is displayed, the user is allowed to answer
   and shown whether they were correct or not. 
   '''
+  @app.route('/quizzes', methods=['POST'])
+  def get_quiz_questions():
+    try:
+
+      body = request.get_json()
+
+      if not ('quiz_category' in body and 'previous_questions' in body):
+          abort(422)
+
+      category = body.get('quiz_category')
+      previous_questions = body.get('previous_questions')
+
+      if category['type'] == 'click':
+        available_questions = Question.query.filter(Question.id.notin_((previous_questions))).all()
+
+      else:
+        available_questions = Question.query.filter_by(category=category['id']).filter(Question.id.notin_((previous_questions))).all()
+
+      new_question = available_questions[random.randrange(0, len(available_questions))].format() if len(available_questions) > 0 else None
+
+      return jsonify({
+        'success': True,
+        'question': new_question
+      })
+      
+    except:
+      abort(422)
 
   '''
   @TODO: 
